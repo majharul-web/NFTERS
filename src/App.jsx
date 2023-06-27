c8cb05c3f74bd97b17b521739944c611e8603cce
majharul <majharuljony163@gmail.com>
2023-06-27 13:20:34 +0600

    [Done] header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Home from './pages/home/Home'
 
 export default function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Home />
     </div>
   )
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Appbar from '../../components/header/Navbar';
+
+
+
+const Home = () => {
+    return (
+        <div className='container-fluid'>
+            <Appbar />
+        </div>
+    );
+};
+
+export default Home;
